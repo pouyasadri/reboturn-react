d06430643f094f50fd7faae97b6e7dcd67b58186
pouyasadri <pouyapaghandeh78@gmail.com>
2023-11-28 05:37:31 +0100

    team  responsive

--- components/WhoWeAre/WhoWeAre.tsx
+++ components/WhoWeAre/WhoWeAre.tsx
@@ -4,33 +4,36 @@ import Image from "next/image";
 interface MemberProps {
     name: string;
     role: string;
+    image: string;
 }
 
-const Member: FC<MemberProps> = ({ name, role }) => (
-    <div className={"flex justify-between items-center border-b-[1px] border-black"}>
-        <div className={"flex justify-center items-center "}>
-            <div className={"p-10"}>
-                <Image className={"rounded-3xl aspect-square"} src={"/CTO.jpg"} alt={"texture1"} width={220} height={100}/>
+const Member: FC<MemberProps> = ({ name, role ,image}) => (
+    <div className={"lg:flex lg:justify-between items-center border-b-[1px] border-black p-2.5"}>
+        <div className={"flex justify-start items-center "}>
+            <div className={"lg:p-10 p-5"}>
+                <Image className={"lg:rounded-3xl rounded-xl aspect-square object-cover lg:w-full lg:h-full w-20 h-20"} src={image} alt={`${name}-${role}-image`} width={150} height={100}/>
             </div>
-            <div className={"text-left p-10"}>
-                <h2 className={"text-3xl mb-5 font-semibold"}>{name}</h2>
-                <span className={"mt-5 bg-[#9ae1ef] rounded-full px-3 py-1 text-xl text-center"}>{role}</span>
+            <div className={"text-left lg:p-10 p-5"}>
+                <h2 className={"lg:text-3xl text-base lg:mb-5 mb-2.5 font-semibold"}>{name}</h2>
+                <span className={"lg:mt-5 mt-2.5 bg-[#9ae1ef] rounded-full lg:px-3 lg:py-1 px-1.5 py-0.5 lg:text-xl text-base text-center"}>{role}</span>
             </div>
         </div>
-        <div className={"text-left w-1/3"}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis doloribus facere ipsam ipsum iusto
+        <div className={"text-left lg:w-1/3 font-light text-sm lg:text-base mb-5"}>
+            <p className={"leading-relaxed"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis doloribus facere ipsam ipsum iusto
                 modi obcaecati quasi repellat repudiandae. Eius!
             </p>
-            <button className="text-black text-md mt-5 relative w-20 h-10 overflow-hidden border border-black bg-transparent px-3 text-black rounded-xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-200 hover:text-white hover:before:left-0 hover:before:w-full"><span className="relative z-10">Read it</span></button>
+            {/*<button className="text-md mt-5 relative w-20 h-10 overflow-hidden border border-black bg-transparent px-3 text-black rounded-xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-200 hover:text-white hover:before:left-0 hover:before:w-full"><span className="relative z-10">Read it</span></button>*/}
         </div>
     </div>
 );
 
 const WhoWeAre: FC = () => (
-    <div className={"w-[90%] mx-auto mt-20 text-black"}>
-        <Member name="SINA GANJI" role="CEO" />
-        <Member name="SEYEDPOUYA SADRIFARD" role="CTO" />
-        <Member name="DANIAL POURARAB" role="ALGORITHM ENGINEER" />
+    <div className={"w-[90%] mx-auto lg:mt-20 mt-10 text-black"}>
+        <Member name="SINA GANJI" role="CEO & Founder" image={"/Sina.png"}/>
+        <Member name="SEYEDPOUYA SADRIFARD" role="CTO & Co-Founder" image={"/CTO.jpeg"}/>
+        <Member name="DANIAL POUR ARAB" role="CTO & Co-Founder" image={"/Danial.jpg"}/>
+        <Member name="MITRA POURARAB" role="Designer" image={"/Mitra.png"}/>
+
     </div>
 );
 
