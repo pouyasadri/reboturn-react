6eb69d2ce9f446b2bad4f1cb83047bcb3f7f73f1
pouyasadri <pouyapaghandeh78@gmail.com>
2023-11-30 20:34:33 +0100

    fix

--- components/Works/ContentSection.tsx
+++ components/Works/ContentSection.tsx
@@ -6,7 +6,7 @@ interface ContentSectionProps {
 const ContentSection: FC<ContentSectionProps> = memo(function ContentSection({text}) {
     return (
 
-        <div className="lg:mt-2.5 leading-relaxed text-gray-700 lg:text-xl max-sm:w-[20rem] text-lg max-sm:mx-auto">
+        <div className="lg:mt-2.5 leading-relaxed text-justify text-gray-700 lg:text-xl max-sm:w-[20rem] text-lg max-sm:mx-auto">
             <p>{text}</p>
         </div>
     );
