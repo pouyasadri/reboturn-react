3f3fbfc18e59cd71c18ec615f152f76ec5665822
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-05 08:41:12 +0100

    fix

--- components/Navbar/NavMobile.tsx
+++ components/Navbar/NavMobile.tsx
@@ -56,8 +56,8 @@ const NavMobile = memo(function NavMobile() {
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
                             exit={{opacity: 0}}
-                            transition={{duration: 0.2,delay:0.5}} id={"contactus"} className={"w-[90%] h-[40%] mx-auto rounded-2xl my-10 relative flex items-center justify-center"}>
-                            <video src={"/output.mp4"} className={"rounded-2xl w-[95%] mx-auto h-[90%] object-cover"} autoPlay loop
+                            transition={{duration: 0.2,delay:0.5}} id={"contactus"} className={"w-[100%] h-[40%] mx-auto rounded-2xl my-10 relative flex items-center justify-center"}>
+                            <video src={"/output.mp4"} className={"rounded-2xl w-[100%] mx-auto h-[90%] object-fill"} autoPlay loop
                                    muted playsInline/>
                             <div
                                 className={"absolute bg-[#9ae1ef] cursor-pointer rounded-3xl z-10 text-center hover:bg-[#aee7f2] transition-all duration-200 mx-auto top-10 bottom-0 left-0 right-0 w-40 h-16 p-5 text-black font-medium flex items-center justify-center"}>
