e2b25ee3ef64de6d9e1af6349b94cac7bf0fc905
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-05 17:59:21 +0100

    fix

--- components/Navbar/NavMobile.tsx
+++ components/Navbar/NavMobile.tsx
@@ -21,7 +21,7 @@ const NavMobile = memo(function NavMobile() {
                         animate={{opacity: 1}}
                         exit={{opacity: 0}}
                         transition={{duration: 0.2}}
-                        className="fixed left-0 right-0 top-[5.5rem] p-5 pt-0 bg-[#f2f2f2] z-10 h-[100vh] border-b border-b-white/20"
+                        className="fixed  left-0 right-0 top-[5.5rem] p-5 pt-0 bg-[#f2f2f2] z-40 h-[100vh] border-b border-b-white/20"
                     >
                         <ul className="grid gap-2">
                             {NAV_ITEMS.map((item, index) => {
