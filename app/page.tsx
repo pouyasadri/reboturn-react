aa19a7327bca070a9664493a96583a0e128b019b
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-08 23:28:34 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,29 @@
 "use client"
-import {FC, useCallback, useEffect, useState} from 'react';
+import {FC, useState, useEffect, useCallback, Suspense} from 'react';
 import dynamic from 'next/dynamic';
-import HeroSection from "@/components/HeroSection/HeroSection";
+import {debounce} from 'lodash';
+import {motion} from 'framer-motion';
 import Navbar from "@/components/Navbar/Navbar";
-import {debounce} from "lodash";
-import {motion} from "framer-motion";
+import HeroSection from "@/components/HeroSection/HeroSection";
 
-const FooterSection = dynamic(() => import('@/components/Footer/Footer'));
-const AboutUs = dynamic(() => import('@/components/About/AboutUs'));
-const ScrollComponent = dynamic(() => import('@/components/Works/Works'));
-const WhoWeAre = dynamic(() => import("@/components/WhoWeAre/WhoWeAre"));
-const WhatWeDo = dynamic(() => import('@/components/WhatWeDo/WhatWeDo'), {
-    ssr: false, loading: () => <div>Loading...</div>
-});
-const WhatWeDoMobile = dynamic(() => import('@/components/WhatWeDo/WhatWeDoMobile'), {
-    ssr: false, loading: () => <div>Loading...</div>
-});
-const Home: FC = () => {
+// const HeroSection = dynamic(() => import('@/components/HeroSection/HeroSection'), {ssr: false, loading: () => <div>Loading...</div>});
+// const Navbar = dynamic(() => import('@/components/Navbar/Navbar'), {ssr: false});
+const FooterSection = dynamic(() => import('@/components/Footer/Footer'), {ssr: false});
+const AboutUs = dynamic(() => import('@/components/About/AboutUs'), {ssr: false});
+const ScrollComponent = dynamic(() => import('@/components/Works/Works'), {ssr: false});
+const WhoWeAre = dynamic(() => import('@/components/WhoWeAre/WhoWeAre'), {ssr: false});
+const WhatWeDo = dynamic(() => import('@/components/WhatWeDo/WhatWeDo'), {ssr: false});
+const WhatWeDoMobile = dynamic(() => import('@/components/WhatWeDo/WhatWeDoMobile'), {ssr: false});
+const useWindowSize = () => {
     const [isMobile, setIsMobile] = useState(false);
-    const updateScreenSize = useCallback(debounce(() => {
-        setIsMobile(window.innerWidth <= 786);
-    }, 300), []);
+
+    const updateScreenSize = useCallback(
+        debounce(() => {
+            setIsMobile(window.innerWidth <= 786);
+        }, 300),
+        []
+    );
+
     useEffect(() => {
         window.addEventListener('resize', updateScreenSize);
         updateScreenSize();
@@ -29,29 +32,37 @@ const Home: FC = () => {
             window.removeEventListener('resize', updateScreenSize);
         };
     }, [updateScreenSize]);
+
+    return isMobile;
+};
+
+const Home: FC = () => {
+    const isMobile = useWindowSize();
+
     return (
-        <>
-            <main className="relative w-screen h-screen ">
-                <motion.div initial={{backdropFilter: "blur(0px)"}}
-                            animate={{backdropFilter: "blur(20px)"}}
-                            transition={{duration: 1, delay: 2}} className={"fixed w-screen h-24 z-40"}>
+        <main className="relative w-screen h-screen">
+            <Suspense fallback={<div className={"w-full h-full"}>
+
+                <video src={"/loading.mp4"} className={" w-screen mx-auto rounded-2xl h-full object-cover"}
+                       autoPlay loop
+                       muted playsInline/>
+            </div>}>
+                <motion.div
+                    initial={{backdropFilter: 'blur(0px)'}}
+                    animate={{backdropFilter: 'blur(20px)'}}
+                    transition={{duration: 1, delay: 2}}
+                    className="fixed w-screen h-24 z-40"
+                >
                     <Navbar/>
                 </motion.div>
                 <HeroSection/>
-
                 <AboutUs/>
-
-
                 <ScrollComponent/>
-                <div id="ourinnovations">
-                    {isMobile ? <WhatWeDoMobile/> : <WhatWeDo/>}
-                </div>
-
+                <div id="ourinnovations">{isMobile ? <WhatWeDoMobile/> : <WhatWeDo/>}</div>
                 <WhoWeAre/>
                 <FooterSection/>
-            </main>
-
-        </>
+            </Suspense>
+        </main>
     );
 };
 
