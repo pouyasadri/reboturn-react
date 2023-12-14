8117676f18333e1c9809fbd012fab8b80e18e19d
pouyasadri <pouyapaghandeh78@gmail.com>
2023-12-14 20:08:42 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
-import Head from 'next/head'
 
 const inter = Inter({subsets: ['latin']})
 const viewport = 'width=device-width, initial-scale=1';
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 "use client"
-import {FC, useState, useEffect, useCallback, Suspense} from 'react';
+import {FC, useState, useEffect, useCallback, Fragment} from 'react';
 import dynamic from 'next/dynamic';
 import {debounce} from 'lodash';
 import {motion} from 'framer-motion';
 import Navbar from "@/components/Navbar/Navbar";
 import HeroSection from "@/components/HeroSection/HeroSection";
 
-// const HeroSection = dynamic(() => import('@/components/HeroSection/HeroSection'), {ssr: false, loading: () => <div>Loading...</div>});
-// const Navbar = dynamic(() => import('@/components/Navbar/Navbar'), {ssr: false});
 const FooterSection = dynamic(() => import('@/components/Footer/Footer'), {ssr: false});
 const AboutUs = dynamic(() => import('@/components/About/AboutUs'), {ssr: false});
 const ScrollComponent = dynamic(() => import('@/components/Works/Works'), {ssr: false});
 const WhoWeAre = dynamic(() => import('@/components/WhoWeAre/WhoWeAre'), {ssr: false});
 const WhatWeDo = dynamic(() => import('@/components/WhatWeDo/WhatWeDo'), {ssr: false});
 const WhatWeDoMobile = dynamic(() => import('@/components/WhatWeDo/WhatWeDoMobile'), {ssr: false});
+
 const useWindowSize = () => {
     const [isMobile, setIsMobile] = useState(false);
 
@@ -38,23 +37,32 @@ const useWindowSize = () => {
 
 const Home: FC = () => {
     const isMobile = useWindowSize();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
-        <main className="relative w-screen h-screen">
-
-            <Suspense fallback={
+        <Fragment>
+            {isLoading &&
                 <div className={"bg-[#f4f4f4] lg:w-screen flex justify-center items-center h-screen"}>
                     <video src={"/loading2.mp4"}
                            className={"w-96 h-96 mx-auto my-0 rounded-2xl object-contain"}
                            preload="auto"
                            autoPlay={true} loop={true} controls={false}
                            muted={true} playsInline={true}/>
-                </div>}>
+                </div>}
 
+            {!isLoading && <main className="relative w-screen h-screen">
                 <motion.div
                     initial={{backdropFilter: 'blur(0px)', WebkitBackdropFilter: 'blur(0px)'}}
                     animate={{backdropFilter: 'blur(30px)', WebkitBackdropFilter: 'blur(30px)'}}
-                    transition={{duration: 2}}
+                    transition={{duration: 2,delay:2}}
                     className="fixed w-screen h-24 z-40"
                 >
                     <Navbar/>
@@ -65,8 +73,8 @@ const Home: FC = () => {
                 <div id="ourinnovations">{isMobile ? <WhatWeDoMobile/> : <WhatWeDo/>}</div>
                 <WhoWeAre/>
                 <FooterSection/>
-            </Suspense>
-        </main>
+            </main>}
+        </Fragment>
     );
 };
 
